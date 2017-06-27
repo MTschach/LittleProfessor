f0bb8d2278220c5ce97aa3e114fa385f7a64e957
michael <michael@MT-2.MTHOME>
2017-06-27 06:08:08 +0200

    Subtraktion implementiert

--- src/de/mss/littleprofessor/math/tasktype/Subtraktion.java
+++ src/de/mss/littleprofessor/math/tasktype/Subtraktion.java
@@ -1,12 +1,26 @@
 package de.mss.littleprofessor.math.tasktype;
 
+import java.math.BigInteger;
+
+import de.mss.littleprofessor.math.MathTask;
 import de.mss.littleprofessor.plugin.Task;
 import de.mss.littleprofessor.plugin.TaskType;
 
 public class Subtraktion extends TaskType {
    @Override
    public Task generateTask() {
-      // TODO Auto-generated method stub
-      return null;
+      MathTask t = new MathTask();
+      
+      BigInteger range = this.upperLimit.subtract(this.lowerLimit);
+      BigInteger a = range.add(BigInteger.ONE).multiply(new BigInteger("" +(long) Math.random()));
+      BigInteger b = a.add(BigInteger.ONE).multiply(new BigInteger("" +(long) Math.random()));
+      BigInteger c = a.subtract(b).subtract(this.lowerLimit);
+      a = a.subtract(this.lowerLimit);
+      b = b.subtract(lowerLimit);
+      
+      t.setTask(a + " - " + b);
+      t.setResult("" + c);
+      
+      return t;
    }
 }
