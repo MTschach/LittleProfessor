e190339cc762e16cfa1cb1de8b5bff41f3421632
Michael Tschach <Michael.Tschach@xcom.de>
2018-08-29 08:36:39 +0200

    switch to gradle build

--- src/main/java/de/mss/littleprofessor/app/LittleProfessor.java
+++ src/main/java/de/mss/littleprofessor/app/LittleProfessor.java
@@ -29,134 +29,129 @@ import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 
 import de.mss.littleprofessor.plugin.PluginInfo;
-import de.mss.littleprofessor.plugin.SchoolGrade;
 import de.mss.littleprofessor.plugin.Task;
 import de.mss.littleprofessor.plugin.TaskType;
 import de.mss.utils.StopWatch;
 
 public class LittleProfessor extends JFrame implements WindowListener, MouseListener, ActionListener, KeyListener {
-   
-   protected JComboBox<String> comboSchoolgrade = null;
-   protected JTextField textNumberOfTasks       = null;
-   
-   protected List<PluginInfo> loadedPlugins     = null;
-   
-   protected JTabbedPane tabs = null;
-   
-   protected ArrayList<Task>taskList            = null;
-   
-   protected JLabel currentTaskText             = null;
-   protected JTextField currentResult           = null;
-   
-   protected java.util.Date startDate           = null;
-   
-   protected int currentTask                    = 0;
-   
-   protected StopWatch stopWatch                = new StopWatch();
-   
-   protected JLabel resultLabel                 = new JLabel();
-   
-   protected int maxWidth                       = 600;
-   protected int maxHeight                      = 520;
-   
-   protected JTable resultTable                 = null;
-   
+
+   protected JComboBox<String> comboSchoolgrade  = null;
+   protected JTextField        textNumberOfTasks = null;
+
+   protected List<PluginInfo>  loadedPlugins     = null;
+
+   protected JTabbedPane       tabs              = null;
+
+   protected ArrayList<Task>   taskList          = null;
+
+   protected JLabel            currentTaskText   = null;
+   protected JTextField        currentResult     = null;
+
+   protected java.util.Date    startDate         = null;
+
+   protected int               currentTask       = 0;
+
+   protected StopWatch         stopWatch         = new StopWatch();
+
+   protected JLabel            resultLabel       = new JLabel();
+
+   protected int               maxWidth          = 600;
+   protected int               maxHeight         = 520;
+
+   protected JTable            resultTable       = null;
+
    /**
     * 
     */
-   private static final long serialVersionUID = -7067067848699201534L;
+   private static final long   serialVersionUID  = -7067067848699201534L;
 
-   public LittleProfessor (String args[])
-   {
+
+   public LittleProfessor(String args[]) {
       init(args);
    }
-   
-   
-   protected void deinit()
-   {
+
+
+   protected void deinit() {
       this.dispose();
    }
-   
-   protected void init(String args[])
-   {
+
+
+   protected void init(String args[]) {
       loadPlugins();
-      
+
       this.setVisible(false);
       this.setTitle("LittleProfessor");
       this.setSize(new Dimension(this.maxWidth, this.maxHeight));
-      
+
       this.setLayout(null);
-      
+
       this.addWindowListener(this);
       this.addMouseListener(this);
-      
+
       initTabs();
-      
+
       this.getContentPane().add(this.tabs);
-      
+
       this.setVisible(true);
    }
-   
-   
-   
-   protected void initTabs()
-   {
+
+
+   protected void initTabs() {
       this.tabs = new JTabbedPane();
-      this.tabs.setBounds(5, 5, this.maxWidth-10, this.maxHeight-40);
-      
+      this.tabs.setBounds(5, 5, this.maxWidth - 10, this.maxHeight - 40);
+
       JPanel p = new JPanel();
-      p.setBounds(5, 5, this.tabs.getWidth() - 10, this.tabs.getHeight()-20);
+      p.setBounds(5, 5, this.tabs.getWidth() - 10, this.tabs.getHeight() - 20);
       p.setLayout(null);
       p.add(getPanelCommons());
       p.add(getPanelLectures());
-      
+
       JButton cont = new JButton("Start");
-      cont.setBounds(5, 375, p.getWidth()-10, 30);
+      cont.setBounds(5, 375, p.getWidth() - 10, 30);
       cont.addActionListener(this);
       cont.setActionCommand("startTasks");
-      
+
       p.add(cont);
-      
+
       tabs.addTab("Auswahl", p);
-      
+
       p = new JPanel();
-      
+
       tabs.addTab("Aufgaben", initTaskPanel());
-      
-      
+
+
       p = new JPanel();
-      
+
       tabs.addTab("Ergebnis", initResultPanel());
-      
+
       tabs.setEnabled(false);
       tabs.setSelectedIndex(0);
    }
-   
-   
-   
+
+
    private JPanel initResultPanel() {
       JPanel resultPanel = new JPanel();
       resultPanel.setLayout(null);
-      resultPanel.setBounds(5, 5, this.tabs.getWidth()-10, this.tabs.getHeight()-20);
-      
+      resultPanel.setBounds(5, 5, this.tabs.getWidth() - 10, this.tabs.getHeight() - 20);
+
       resultLabel.setText("");
-      resultLabel.setBounds(5,10,resultPanel.getWidth()-10, 40);
+      resultLabel.setBounds(5, 10, resultPanel.getWidth() - 10, 40);
       resultPanel.add(resultLabel);
-      
+
       resultTable = new JTable();
-      
+
       JScrollPane scroll = new JScrollPane(resultTable);
-      scroll.setBounds(5, 60, resultPanel.getWidth()-10, 170);
-      
+      scroll.setBounds(5, 60, resultPanel.getWidth() - 10, 170);
+
       resultPanel.add(scroll);
-      
+
       JButton redo = new JButton("Nochmal");
       redo.addActionListener(this);
       redo.setActionCommand("redo");
-      redo.setBounds(5, 250, resultPanel.getWidth()-10, 40);
-      
+      redo.setBounds(5, 250, resultPanel.getWidth() - 10, 40);
+
       resultPanel.add(redo);
-      
+
       return resultPanel;
    }
 
@@ -164,141 +159,133 @@ public class LittleProfessor extends JFrame implements WindowListener, MouseList
    private JPanel initTaskPanel() {
       JPanel p = new JPanel();
       p.setLayout(null);
-      p.setBounds(5, 5, this.tabs.getWidth()-10, this.tabs.getHeight()-20);
-      
+      p.setBounds(5, 5, this.tabs.getWidth() - 10, this.tabs.getHeight() - 20);
+
       this.currentTaskText = new JLabel("");
-      this.currentTaskText.setBounds(10, 10, p.getWidth()-20, 140);
-      
+      this.currentTaskText.setBounds(10, 10, p.getWidth() - 20, 140);
+
       p.add(this.currentTaskText);
-      
+
       this.currentResult = new JTextField(70);
-      this.currentResult.setBounds(10, 160, p.getWidth()-20, 140);
+      this.currentResult.setBounds(10, 160, p.getWidth() - 20, 140);
       this.currentResult.addKeyListener(this);
-      
+
       p.add(this.currentResult);
-      
+
       JButton cont = new JButton("Weiter");
-      cont.setBounds(10, 320, p.getWidth()-20, 30);
+      cont.setBounds(10, 320, p.getWidth() - 20, 30);
       cont.addActionListener(this);
       cont.setActionCommand("nextTask");
-      
+
       p.add(cont);
-      
+
       return p;
    }
 
 
    private void loadPlugins() {
       this.loadedPlugins = new ArrayList<>();
-      
+
       String[] pl = {"de.mss.littleprofessor.math.PluginInfo"};
-      
-      for (String  p : pl)
-      {
+
+      for (String p : pl) {
          try {
-            PluginInfo pluginInfo = (PluginInfo) Class.forName(p).newInstance();
+            PluginInfo pluginInfo = (PluginInfo)Class.forName(p).newInstance();
             loadedPlugins.add(pluginInfo);
-         } catch (InstantiationException e) {
+         }
+         catch (InstantiationException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
-         } catch (IllegalAccessException e) {
+         }
+         catch (IllegalAccessException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
-         } catch (ClassNotFoundException e) {
+         }
+         catch (ClassNotFoundException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
          }
       }
-      
+
    }
 
 
-   protected JPanel getPanelCommons()
-   {
+   protected JPanel getPanelCommons() {
       JPanel panelCommons = new JPanel();
       panelCommons.setLayout(null);
       panelCommons.setBorder(new TitledBorder("Allgemein"));
-      panelCommons.setBounds(5, 5, this.tabs.getWidth()-10,80);
-      
-      JLabel l = new JLabel("Klassenstufe");
+      panelCommons.setBounds(5, 5, this.tabs.getWidth() - 10, 80);
+
+      JLabel l = new JLabel("Aufgabenzahl");
       l.setBounds(5, 15, 150, 25);
+
       panelCommons.add(l);
-      
-      comboSchoolgrade = new JComboBox<>();
-      comboSchoolgrade.setBounds(170, 15, 160, 25);
-      comboSchoolgrade.addActionListener(this);
-      comboSchoolgrade.setActionCommand("changeSchoolgrade");
-      
-      for (SchoolGrade s : SchoolGrade.values())
-         comboSchoolgrade.addItem(s.getName());
-      
-      panelCommons.add(comboSchoolgrade);
-      
-      l = new JLabel("Aufgabenzahl");
-      l.setBounds(5, 45, 150, 25);
-      
-      panelCommons.add(l);
-      
+
       textNumberOfTasks = new JTextField(10);
-      textNumberOfTasks.setBounds(170, 45, 160, 25);
+      textNumberOfTasks.setBounds(170, 15, 160, 25);
       textNumberOfTasks.setText("10");
-      
+
       panelCommons.add(textNumberOfTasks);
-      
+
       return panelCommons;
    }
-   
-   
-   protected JTabbedPane getPanelLectures()
-   {
+
+
+   protected JTabbedPane getPanelLectures() {
       JTabbedPane panelLectures = new JTabbedPane(JTabbedPane.TOP, JTabbedPane.SCROLL_TAB_LAYOUT);
-      panelLectures.setBounds(5, 90, this.tabs.getWidth()-10, 280);
+      panelLectures.setBounds(5, 90, this.tabs.getWidth() - 10, 280);
       panelLectures.setBorder(new TitledBorder("Fächer"));
-      
+
       panelLectures.removeAll();
-      for (PluginInfo pluginInfo : loadedPlugins)
-      {
-         panelLectures.addTab(pluginInfo.getPluginName(),pluginInfo.getTaskConfigPanel());
+      for (PluginInfo pluginInfo : loadedPlugins) {
+         panelLectures.addTab(pluginInfo.getPluginName(), pluginInfo.getTaskConfigPanel());
       }
-      
+
       return panelLectures;
    }
+
 
    @Override
    public void mouseClicked(MouseEvent e) {
       // TODO Auto-generated method stub
-      
+
    }
+
 
    @Override
    public void mousePressed(MouseEvent e) {
       // TODO Auto-generated method stub
-      
+
    }
+
 
    @Override
    public void mouseReleased(MouseEvent e) {
       // TODO Auto-generated method stub
-      
+
    }
+
 
    @Override
    public void mouseEntered(MouseEvent e) {
       // TODO Auto-generated method stub
-      
+
    }
+
 
    @Override
    public void mouseExited(MouseEvent e) {
       // TODO Auto-generated method stub
-      
+
    }
+
 
    @Override
    public void windowOpened(WindowEvent e) {
       // TODO Auto-generated method stub
-      
+
    }
+
 
    @Override
    public void windowClosing(WindowEvent e) {
@@ -306,34 +293,40 @@ public class LittleProfessor extends JFrame implements WindowListener, MouseList
       deinit();
    }
 
+
    @Override
    public void windowClosed(WindowEvent e) {
       // TODO Auto-generated method stub
    }
 
+
    @Override
    public void windowIconified(WindowEvent e) {
       // TODO Auto-generated method stub
-      
+
    }
+
 
    @Override
    public void windowDeiconified(WindowEvent e) {
       // TODO Auto-generated method stub
-      
+
    }
+
 
    @Override
    public void windowActivated(WindowEvent e) {
       // TODO Auto-generated method stub
-      
+
    }
+
 
    @Override
    public void windowDeactivated(WindowEvent e) {
       // TODO Auto-generated method stub
-      
+
    }
+
 
    public static void main(String[] args) {
       new LittleProfessor(args);
@@ -343,16 +336,13 @@ public class LittleProfessor extends JFrame implements WindowListener, MouseList
    @Override
    public void actionPerformed(ActionEvent e) {
       String cmd = e.getActionCommand();
-      
-      if ("changeSchoolgrade".equals(cmd))
-         changeSchoolgrade();
-      
-      else if ("startTasks".equals(cmd))
+
+      if ("startTasks".equals(cmd))
          startTasks();
-      
+
       else if ("nextTask".equals(cmd))
          nextTask();
-      
+
       else if ("redo".equals(cmd))
          redo();
    }
@@ -364,22 +354,20 @@ public class LittleProfessor extends JFrame implements WindowListener, MouseList
 
 
    private void nextTask() {
-      if (this.currentTask >= 0)
-      {
+      if (this.currentTask >= 0) {
          stopWatch.stop();
          this.taskList.get(this.currentTask).setGivenResult(this.currentResult.getText());
          this.taskList.get(this.currentTask).setTaskDuration(stopWatch.getElapsedTime());
       }
-      
+
       int maxTasks = this.taskList.size();
-      this.currentTask++;
-      
-      if (this.currentTask >= maxTasks)
-      {
+      this.currentTask++ ;
+
+      if (this.currentTask >= maxTasks) {
          allTasksDone();
          return;
       }
-      
+
       this.currentTaskText.setText(this.taskList.get(this.currentTask).getTask());
       this.currentResult.setText("");
       stopWatch.reset();
@@ -389,40 +377,38 @@ public class LittleProfessor extends JFrame implements WindowListener, MouseList
 
    private void allTasksDone() {
       this.tabs.setSelectedIndex(2);
-      
+
       long duration = 0;
       int correctTasks = 0;
-      for (Task t : this.taskList)
-      {
+      for (Task t : this.taskList) {
          duration += t.getTaskDuration();
          if (t.isCorrect())
-            correctTasks++;
+            correctTasks++ ;
       }
-      
-      this.resultLabel.setText(getResultText(correctTasks, taskList.size(), duration, SchoolGrade.getByName((String)this.comboSchoolgrade.getSelectedItem())));
-      
+
+      this.resultLabel.setText(getResultText(correctTasks, taskList.size(), duration));
+
       DefaultTableModel dataModel = new DefaultTableModel(taskList.size(), 4);
-      
-      dataModel.setColumnIdentifiers(new String[]{"Aufgabe", "gegebene Lösung", "Lösung", "", "Zeit"});
-      
-      int i=0;
-      for (Task t : this.taskList)
-      {
+
+      dataModel.setColumnIdentifiers(new String[] {"Aufgabe", "gegebene Lösung", "Lösung", "", "Zeit"});
+
+      int i = 0;
+      for (Task t : this.taskList) {
          dataModel.setValueAt(t.getTask(), i, 0);
          dataModel.setValueAt(t.getGivenResult(), i, 1);
          dataModel.setValueAt(t.getResult(), i, 2);
          dataModel.setValueAt((t.isCorrect() ? "richtig" : "falsch"), i, 3);
          dataModel.setValueAt(formatDuration(t.getTaskDuration()), i, 4);
-         
-         i++;
+
+         i++ ;
       }
-      
+
       this.resultTable.setModel(dataModel);
       this.resultTable.setDefaultRenderer(Object.class, new MyTableCellRender());
    }
 
 
-   private String getResultText(int correctTasks, int size, long duration, SchoolGrade schoolgrade) {
+   private String getResultText(int correctTasks, int size, long duration) {
       StringBuilder sb = new StringBuilder("Du hast ");
       sb.append(correctTasks);
       sb.append(" von ");
@@ -430,26 +416,11 @@ public class LittleProfessor extends JFrame implements WindowListener, MouseList
       sb.append(" Aufgabe(n) richtig.\nDafür hast du ");
       sb.append(formatDuration(duration));
       sb.append(" benötigt.");
-      
-      switch (schoolgrade)
-      {
-         case NONE:
-         case PRESCHOOL:
-         case FIRSTCLASS: break;
-         
-         case SECONDCLASS:
-            sb.append("\nDas wäre eine ");
-            sb.append(calculateNote(correctTasks, size));
-            sb.append(" gewesen.");
-            break;
-            
-         default:
-            sb.append("\ndas ist eine ");
-            sb.append(calculateNote(correctTasks, size));
-            sb.append(".");
-            break;
-            
-      }
+
+      sb.append("\nDas wäre eine ");
+      sb.append(calculateNote(correctTasks, size));
+      sb.append(" gewesen.");
+
       return sb.toString();
    }
 
@@ -462,66 +433,54 @@ public class LittleProfessor extends JFrame implements WindowListener, MouseList
    private String formatDuration(long duration) {
       if (duration <= 0)
          return "0 ms";
-      
-      long ms = duration%1000;
+
+      long ms = duration % 1000;
       duration /= 1000;
-      long s  = duration%60;
+      long s = duration % 60;
       duration /= 60;
-      long m  = duration%60;
+      long m = duration % 60;
       duration /= 60;
-      
+
       return String.format("%d:%02d:%02d.%03d", duration, m, s, ms);
-      
+
    }
 
 
    private void startTasks() {
-      
+
       ArrayList<TaskType> availTasks = new ArrayList<>();
-      
-      for (PluginInfo p : this.loadedPlugins)
-      {
+
+      for (PluginInfo p : this.loadedPlugins) {
          availTasks.addAll(p.getSelectedTasks());
       }
-      
-      
+
+
       taskList = new ArrayList<>();
       int count = Integer.parseInt(this.textNumberOfTasks.getText());
-      
-      for (int i=0; i<count; i++)
-      {
+
+      for (int i = 0; i < count; i++ ) {
          taskList.add(availTasks.get(new Random().nextInt(availTasks.size())).generateTask());
       }
-      
+
       this.tabs.setSelectedIndex(1);
-      
+
       this.currentTask = -1;
       nextTask();
       this.startDate = new java.util.Date();
    }
 
 
-   private void changeSchoolgrade() {
-      SchoolGrade sg = SchoolGrade.getByName(comboSchoolgrade.getSelectedItem().toString());
-      
-      for (PluginInfo pi : loadedPlugins)
-      {
-         pi.doPreselection(sg);
-      }
-   }
-
-
    @Override
    public void keyTyped(KeyEvent e) {
       // TODO Auto-generated method stub
-      
+
    }
 
 
    @Override
    public void keyPressed(KeyEvent e) {
       // TODO Auto-generated method stub
-      
+
    }
 
 
@@ -532,22 +491,23 @@ public class LittleProfessor extends JFrame implements WindowListener, MouseList
             nextTask();
    }
 
-   
+
    private class MyTableCellRender extends DefaultTableCellRenderer {
-	   
-	   public MyTableCellRender() {
-		   setOpaque(true);
-	   }
-	     
-	   public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-		   if ("richtig".equals(table.getValueAt(row, 3))) {
-			   setBackground(Color.GREEN);
-		   }
-		   else {
-			   setBackground(Color.RED);
-		   }
-		   setText(value !=null ? value.toString() : "");
-		   return this;
-	  	}
-   } 
+
+      public MyTableCellRender() {
+         setOpaque(true);
+      }
+
+
+      public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+         if ("richtig".equals(table.getValueAt(row, 3))) {
+            setBackground(Color.GREEN);
+         }
+         else {
+            setBackground(Color.RED);
+         }
+         setText(value != null ? value.toString() : "");
+         return this;
+      }
+   }
 }
--- src/main/java/de/mss/littleprofessor/plugin/PluginInfo.java
+++ src/main/java/de/mss/littleprofessor/plugin/PluginInfo.java
@@ -5,9 +5,12 @@ import java.util.List;
 import javax.swing.JPanel;
 
 public interface PluginInfo extends de.mss.plugin.PluginInfo {
-   public List<TaskType> getAvailableTaskTypes(SchoolGrade s);
+
+   public List<TaskType> getAvailableTaskTypes();
+
+
    public List<TaskType> getSelectedTasks();
-   public void doPreselection(SchoolGrade s);
-   
+
+
    public JPanel getTaskConfigPanel();
 }
